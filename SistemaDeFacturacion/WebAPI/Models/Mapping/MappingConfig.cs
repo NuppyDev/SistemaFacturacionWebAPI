cd0eccf4f3411d07ed00914c3768cfe361adfc97
StephTeno <106124803+StephTeno@users.noreply.github.com>
2023-06-05 15:11:30 -0600

    Inicializar Controller Historical

--- SistemaDeFacturacion/WebAPI/Models/Mapping/MappingConfig.cs
+++ SistemaDeFacturacion/WebAPI/Models/Mapping/MappingConfig.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using WebAPI.Models.Clases;
 using WebAPI.Models.Dto.Base;
+using WebAPI.Models.Dto.Create;
+using WebAPI.Models.Dto.Update;
 
 namespace WebAPI.Models.Mapping
 {
@@ -8,7 +10,14 @@ namespace WebAPI.Models.Mapping
     {
         public MappingConfig() 
         {
+            CreateMap<Waiters, WaitersDto>().ReverseMap();
             CreateMap<Waiters, WaitersCreateDto>().ReverseMap();
+            CreateMap<Waiters, WaitersUpdateDto>().ReverseMap();
+
+            CreateMap<Tables, TablesDto>().ReverseMap();
+            CreateMap<Tables, TablesCreateDto>().ReverseMap();
+            CreateMap<Tables, TablesUpdateDto>().ReverseMap();
+
         }
     }
 }
